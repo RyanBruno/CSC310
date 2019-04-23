a8a4279f23f54fc1912bedf256ac6ac57a429dc3
Ryan Bruno <ryanbruno506@gmail.com>
2019-04-23 16:27:06 -0400

    Removed build files

--- build/extern/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ryan/workspace/CSC310/extern/googletest/googlemock
-# Build directory: /home/ryan/workspace/CSC310/build/extern/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- build/extern/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ryan/workspace/CSC310/extern/googletest/googletest
-# Build directory: /home/ryan/workspace/CSC310/build/extern/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
