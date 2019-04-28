2a4a03d33d664f45c5d070d40c935265070569e8
Ryan Bruno <ryanbruno506@gmail.com>
2019-04-28 14:09:19 -0400

    WIP

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -4,22 +4,7 @@
 #include "algo.hpp"
 #include "algo_test.cpp"
 
-/*
-template <int I>
-void generate_polygon(
-        std::array<Point, I>& polygon, const int& n)
-{
-    std::for_each(polygon.begin(), polygon.end(), [&](Point& p)
-    {
-        std::srand(std::time(nullptr));
-        p.x = std::rand() % 1000;
-        EXPECT_TRUE(false) << p.x;
-        p.y = std::rand() % n;
-        EXPECT_TRUE(false) << p.y;
-    });
-}
-
-#include "emp_test.cpp"*/
+#include "emp_test.cpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
